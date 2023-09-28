d49edbb41771c581bbb9eead48b58c0c9293135b
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-09-28 22:18:48 +0530

    Create 4.literals.java

---
+++ youtube.java/4.literals.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+literals:
+        collection of class is called as literals
+types:
+        interger
+        boolean
+        Character
+        
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    int ticketPrice = 800;
+	    
+		System.out.println("\"Hello World\"");// string literals
+		
+		int a= 010;
+	    System.out.println(a);// octal Number
+	    
+	   int b= oxA; 
+	   System.out.println(b);// hexa decimal 
+	   
+	   long c=7l;
+	   System.out.println(c);// consider as a long value
+	   	
+	   
+	   int d=ob00111
+	   System.out.println(d);// consider as a boolean value
+	   
+	   int e=1,23,456,789;// java la comma la use pana kudathu// wrong
+	   int e=1_23_456_789;// correct
+	   System.out.println(e);
+	   
+	   //floating point
+	   float pi=3.14159;// standard notation 
+	   float pi=315159e-05;// scientific notation 
+	   System.out.println(pi);
+	   
+	   
+	   
+	   
+	}
+}
