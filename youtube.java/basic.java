ab72470e4d9e65d2397608789998a9dcb4c77b3a
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-09-28 22:14:53 +0530

    Create 1.basic.java

---
+++ youtube.java/basic.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+basic program 
+
+*******************************************************************************/
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+/*
+print streams
+java input and output this is a libary
+It ois not a improtant because this is basic libary are already imported
+*/
+
+/*
+public- visible to eveyone
+class Main- name of the class
+*/
+
+/*
+pubilc-asccessable this class to everyone
+Static-oru clasa la method use pananu na we want to create object and then we will use the method.
+        but static use pani apidiya class la use panalam wothput creating object.
+void-return nothing (no return type).
+main - name of the class
+String[]-Sequences of Character.
+args - parameter to pass 
+*/
+
+
+/*
+this line is used to print the string which are present in the double codes "  "
+*/
