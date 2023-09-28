513bdfe3a6afc4771536da1c96f57dfb1cfd7902
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-09-28 22:16:04 +0530

    Create 2.Hello world.java

---
+++ youtube.java/2.Hello world.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	    System.out.print("Hello World");
+	}
+}
+/*
+println - is used to print the string and oes to nect line
+*/
+
