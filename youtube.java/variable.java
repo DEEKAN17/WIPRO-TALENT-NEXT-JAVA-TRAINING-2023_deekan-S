6d269e79d25ec2d081f8c6588653f5a7082c8e64
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-09-28 22:17:36 +0530

    Create variable.java

---
+++ youtube.java/variable.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+Variable in java 
+Syntax:
+        datatype ticketPrice;
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int ticketPrice = 800;
+	    boolean b = true;
+	    char c ='t';
+	    
+		System.out.println(ticketPrice);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
+
